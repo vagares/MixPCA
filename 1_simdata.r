deb63c4533789a68b3eb6785f717625fc4f58913
mgiacofci <55752120+mgiacofci@users.noreply.github.com>
2022-10-23 16:24:28 +0200

    reprise code (vendredi 21/10)

--- 1_simdata.r
+++ 1_simdata.r
@@ -15,6 +15,7 @@ data = function(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
   theta2 = (mean(apply(mu,2,sd))/SNR2)^2
   y = NULL
   x = NULL
+  g = NULL
   for (k in (1:K)){
     nk=n[k]
     betak=beta
@@ -34,10 +35,13 @@ data = function(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
     yk=rep(1,nk)%*%t(muk)+t(Qk%*%t(alphaik))+matrix(rnorm(p*nk,mean=0,sd=sqrt(theta2)),ncol=p,nrow=nk)
     y=rbind(y,yk)
     x=rbind(x,xk)
+    g=c(g,rep(k,nk))
     #matplot(muk+as.vector(Qk%*%alphaik)+matrix(rnorm(p*K,mean=0,sd=sqrt(theta2)),ncol=K,nrow=p),type="l",lty=3)
     #matlines(muk+as.vector(Qk%*%alphaik),lty=1)
     #matlines(muk,lty=1)
   }
+    data = data.frame(y = y,x=x,g=g)
+    colnames(data) = c(paste("y",1:p,sep=""), paste("x",1:nx,sep=""),"g")
   return(data)
 }
 
@@ -48,4 +52,5 @@ data1=data(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
                 SNR1 = 100,
                 SNR2 = 3)
 
+p=10
 matplot(t(data1[,1:p]),col=data1$g,type="l")
--- 2_estimators.r
+++ 2_estimators.r
@@ -5,54 +5,51 @@
 #########################################################################
 
 Estep = function(n,K = 3,q = 4,p = 10,nx=4,
-                 pi,mu,beta, theta2=1,
+                 pik,mu,beta, theta2=1,
                  sigma2=1,
+                 x=x,y=y,
                  Q=Q,
                  C=C){
     alphai =list()
     alphai2 =list()
     alphai22 =list()
     alphai2Q =list()
-    taui =list()
     nn=sum(n)
+    tau = matrix(0,nn,K)
+   
     for (k in 1:K){
       betak=beta[[k]]
       Qk=Q[[k]]
       muk=mu[[k]]
       ez= muk%*%t(rep(1,nn)) + Qk%*%betak%*%t(x)
       vz = theta2 * (Qk%*%t(Qk))+sigma2*diag(p)
-      taui[[k]] = numeric(nn)
       for (i in (1:nn)){
-      taui[[k]][i] = (dmvnorm(y[i,], mean = ez[,i], sigma = vz))*pi[k]}
+      tau[i,k] = dmvnorm(y[i,], mean = ez[,i], sigma = vz)*pik[k]}
       alphai[[k]] = betak%*%t(x) + theta2*t(Qk) %*%solve(theta2*Qk%*%(t(Qk))+sigma2*diag(p)) %*% (t(y)-muk%*%t(rep(1,nn))-Qk%*%betak%*%t(x))
       alphai22[[k]] = matrix(0,ncol=nn,nrow=q*q)
       alphai2[[k]] = numeric(nn)
       alphai2Q[[k]] = numeric(nn)
       for (i in (1:nn)){
-        alphai2[[k]][i] = sum(diag(theta2*solve(diag(q)+(theta2/sigma2)*t(Qk)%*%Qk)))+t(alphai[[k]][,i])%*%alphai[[k]][,i]
-        alphai2Q[[k]][i] = sum(diag(theta2*Qk%*%solve(diag(q)+(theta2/sigma2)*t(Qk)%*%Qk)%*%t(Qk)))+t(alphai[[k]][,i])%*%t(Qk)%*%Qk%*%alphai[[k]][,i]
+        alphai2[[k]][i] = sum(diag(theta2*solve(diag(q)+(theta2/sigma2)*t(Qk)%*%Qk)))+sum(alphai[[k]][,i]^2)
+        alphai2Q[[k]][i] = sum(diag(theta2*Qk%*%solve(diag(q)+(theta2/sigma2)*t(Qk)%*%Qk)%*%t(Qk)))+sum(Qk%*%alphai[[k]][,i]^2)
         alphai22[[k]][,i] = as.vector(theta2*solve(diag(q)+(theta2/sigma2)*t(Qk)%*%Qk)+alphai[[k]][,i]%*%t(alphai[[k]][,i]))
       }
     }
-    for (i in (1:nn)){
-      Staui = 0
-      for (k in 1:K){
-        Staui = Staui +taui[[k]][i] 
-      }
-      for (k in 1:K){taui[[k]][i]=taui[[k]][i]/Staui}
-    }
-    return(list(alphai=alphai,alphai2=alphai2,alphai22=alphai22,alphai2Q=alphai2Q,taui=taui))
+    sommetau = apply(tau,1,sum)
+    tau = tau/sommetau
+    return(list(alphai=alphai,alphai2=alphai2,alphai22=alphai22,alphai2Q=alphai2Q,tau=tau))
 }
+
 Mstep = function(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
-                 x,y,z,C,muold,alphai,alphai2,alphai22, alphai2Q,taui){
-  pi = unlist(lapply(taui,mean))
+                 x,y,z,C,muold,alphai,alphai2,alphai22, alphai2Q,tau){
+  pik = apply(tau,2,mean)
   mu = list()
   Q = list()
-  theta2i = numeric(K)
+  theta2i = matrix(0,nn,K)
   sigma2i = numeric(K)
   nn=sum(n)
   for (k in 1:K){
-    tauik=taui[[k]]
+    tauik=tau[,k]
     alphaik = alphai[[k]]
     alphai2k = alphai2[[k]]
     alphai22k = alphai22[[k]]
@@ -65,10 +62,13 @@ Mstep = function(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
     S2=0
     for (i in (1:nn))
     {S1 = S1 + tauik[i] * (x[i,] %*%t(x[i,]))
-    S2 =S2 + tauik[i] * x[i,] %*%t(alphaik[,i])}
-    beta[[k]]= solve(S2)%*%S1
-    betak = matrix(as.numeric(beta[[k]]),q,q)
-    theta2i[k] =  sum(tauik * (alphai2k  -2* x%*%t(betak)%*%alphaik+x%*%t(betak)%*%betak%*%t(x)))
+    S2 =S2 + tauik[i] * x[i,] %*%t(alphaik[,i])
+    }
+    beta[[k]]= solve(S1)%*%S2
+    #betak = matrix(as.numeric(beta[[k]]),q,q)
+    for (i in (1:nn)){
+    theta2i[i,k] =  tauik[i] * (alphai2k[i]  -2* t(x[i,])%*%t(beta[[k]])%*%alphaik[,i]+t(x[i,])%*%t(beta[[k]])%*%beta[[k]]%*%x[i,])
+    }
     
 
     S1=0
@@ -82,49 +82,46 @@ Mstep = function(n=c(100,100,100),K = 3,q = 4,p = 10,nx=4,
   }
   theta2 = (1/(nn*q))*sum(theta2i)
   sigma2 = (1/(nn*p))*sum(sigma2i)
-  return(list(pi=pi,beta = beta, mu=mu,Q=Q,theta2=theta2,sigma2=sigma2))
+  return(list(pik=pik,beta = beta, mu=mu,Q=Q,theta2=theta2,sigma2=sigma2))
 }
 
-estimates = function(data,K=3,maxits=100,tol=0.01, q = 4,p = 10,nx=4){
+estimates = function(data,K=3,maxits=100,tol=0.01, q = 4,nx=4,p=10){}
   y = data[,1:p]
   x = data[,(p+1):(p+nx)]
   #initialisation
   groupe=1:K
   C= sample(groupe,dim(y)[1],replace=TRUE)
-  pi=rep(1/K,K)
-  n = NULL
-  for (k in (1:K)){n = c(n,sum(as.numeric(C==k)))}
-  nx=dim(x)[2]
-  #q=dim(y)[2]
-  p=dim(y)[2]
-  pi = c(0.3,0.3,0.3)
+  pik=rep(1/K,K)
+  n = as.numeric(summary(as.factor(C)))
+
   mu = list()
-  for (k in (1:K)){mu[[k]]=mu1[,k]}
-  beta1 = matrix(rnorm(4*4,mean=0,sd=2),ncol=nx)
+  for (k in (1:K)){mu[[k]]=apply(y[which(C==k),],2,mean)}
   beta = list()
-  for (k in (1:K)){beta[[k]]=beta1}
+  for (k in (1:K)){beta[[k]]= matrix(rnorm(q*nx,mean=0,sd=2),ncol=nx)}
   theta2=1
   sigma2=1
-  bfourier = create.fourier.basis(rangeval=c(0,1),nbasis=q)
-  fmat = getbasismatrix(evalarg=seq(0,1,length=p),bfourier)
-  Q1 = gramSchmidt(fmat[,-1])$Q
   Q=list()
-  for (k in (1:K)){Q[[k]]=Q1}
+  for (k in (1:K)){Q[[k]]=diag(p)[,1:q]}
   diff = 10
   iter=0
   while (diff > tol && iter < maxits){
-    old.pi <- pi
+    old.pik <- pik
     old.mu <- mu
     old.Q <- Q
     old.beta <- beta
     old.theta2 <- theta2
     old.sigma2 <- sigma2
-    alphai=Estepresults$alphai;alphai2=Estepresults$alphai2;alphai22=Estepresults$alphai22;alphai2Q=Estepresults$alphai2Q;taui=Estepresults$taui
+    Estepresults = Estep(n,K,q,p,nx,pik,mu,beta,theta2,sigma2,x,y,Q,C)
+    alphai=Estepresults$alphai;alphai2=Estepresults$alphai2;alphai22=Estepresults$alphai22;alphai2Q=Estepresults$alphai2Q;tau=Estepresults$tau
     
-    Mstepresults = Mstep(n,K,q,p,nx=4,x,y,z,C,old.mu,alphai,alphai2,alphai22, alphai2Q,taui)
-    pi=Mstepresults$pi;beta=Mstepresults$beta;mu=Mstepresults$mu;Q=Mstepresults$Q;theta2=Mstepresults$theta2;sigma2=Mstepresults$sigma2
+    Mstepresults = Mstep(n,K,q,p,nx=4,x,y,z,C,old.mu,alphai,alphai2,alphai22, alphai2Q,tau)
+    pik=Mstepresults$pik;beta=Mstepresults$beta;mu=Mstepresults$mu;Q=Mstepresults$Q;theta2=Mstepresults$theta2;sigma2=Mstepresults$sigma2
     diff1=numeric(K)
+    for (k in (1:K)){diff1[k] = sum((pik[k]-old.pik[k])^2)+sum((mu[[k]] - old.mu[[k]])^2)+sum((Q[[k]] - old.Q[[k]])^2) + sum((beta[[k]] - old.beta[[k]])^2)}
+    diff = sum(diff1) + sum((theta2-old.theta2)^2) + sum((sigma2-old.sigma2)^2)
     iter = iter +1
+    
   }
+  return(list(pik=pik,mu=mu,beta=beta,Q=Q,theta2=theta2,sigma2=sigma2))}
   
   
